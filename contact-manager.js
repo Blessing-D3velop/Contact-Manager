1f8e49c901a0be8c059a06dfbde1f262946d4771
Blessing-D3velop <blessingmuthivhi39@gmail.com>
2026-03-12 22:49:05 +0200

    initial commit

---
+++ contact-manager.js
@@ -0,0 +1,128 @@
+let contacts = [
+
+];
+
+let container = document.querySelector('.js-add-contact-container');
+let addContactButtons = document.querySelectorAll('.js-add-contact-button');
+let backdrop = document.querySelector('.js-backdrop');
+let cardsContainer = document.querySelector('.js-cards-grid');
+
+// Function to render cards
+function renderContacts() {
+  cardsContainer.innerHTML = '';
+  contacts.forEach(contact => {
+    let html = `
+      <div class="card-container">
+        <div class ="first-line">
+          <p class ="name-image">
+            <img src="Icons/icons8-contact-24.png" alt="contact-icon" class="name-icon">
+          </p>
+          <p class = "name-text">
+            ${contact.name}
+          </p>
+        </div>
+
+        <div class="second-line">
+          <p class="phone-image">
+            <img src="Icons/icons8-phone-24.png" alt="phone-icon" class="phone-icon">      
+          </p>
+          <p class="phone-text">
+            ${contact.phone}
+          </p>
+        </div>
+
+        <div class="third-line">
+          <p class="email-image">
+            <img src="Icons/icons8-email-24.png" alt="email-icon" class="email-icon">
+          </p>
+          <p class="email-text">
+          ${contact.email}
+          </p>
+        </div>
+
+        <div class="container-card-buttton">
+          <button class="edit-button">
+            <img src="Icons/icons8-edit.svg" alt="edit-icon" class="edit-icon">
+            Edit
+          </button>
+          <button class="delete-button">
+            <img src="Icons/icons8-delete.svg" alt="delete icon" class="delete-icon">
+            Delete
+          </button>
+        </div>
+      </div>
+
+  `;
+    cardsContainer.innerHTML += html;
+  });
+    // Show or hide empty state
+  const emptyState = document.querySelector('.js-empty-state');
+  if (contacts.length > 0) {
+    emptyState.style.display = 'none';
+  } else {
+    emptyState.style.display = 'block';
+  }
+}
+
+// Open form
+addContactButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    backdrop.style.display = 'block';
+    let html = `
+      <div class="contact-form">
+        <div class="contact-headding-container">
+          <p class="contact-headding">Add New Contact</p>
+        </div>
+
+        <p class="label">Name *</p>
+        <input type="text" placeholder="John Doe" class="name-input js-inputName" required>
+        <p class="label">Phone *</p>
+        <input type="text" placeholder="+27 000 000 0000" class="phone-input js-inputPhone" required>
+        <p class="label">Email *</p>
+        <input type="text" placeholder="John@example.com" class="email-input js-inputEmail" required>
+
+        <div class="form-buttons">
+          <button class="add-button">Add</button>
+          <button class="cancel-button">Cancel</button>
+        </div>
+
+        <p class="js-output"></p>
+      </div>
+    `;
+    container.innerHTML = html;
+
+    // Cancel button
+    document.querySelector('.cancel-button').addEventListener('click', () => {
+      container.innerHTML = '';
+      backdrop.style.display = 'none';
+    });
+
+    // Add button
+    const addContactTwo = document.querySelector('.add-button');
+    let output = document.querySelector('.js-output');
+
+    addContactTwo.addEventListener('click', () => {
+      let name = document.querySelector('.js-inputName').value;
+      let email = document.querySelector('.js-inputEmail').value;
+      let phone = document.querySelector('.js-inputPhone').value;
+
+      output.textContent = '';
+
+      if (!name.trim()) {
+        output.textContent = 'Invalid Name';
+        return;
+      } else if (!email.includes('@')) {
+        output.textContent = 'Invalid Email';
+        return;
+      } else if (!phone || phone.replace(/\D/g,'').length !== 11) {
+        output.textContent = 'Phone number must be 10 digits';
+        return;
+      }
+
+      contacts.push({name, email, phone});
+      container.innerHTML = '';
+      backdrop.style.display = 'none';
+      renderContacts();
+    });
+  });
+});
